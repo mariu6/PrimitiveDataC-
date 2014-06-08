1eefec996c3d45f7a6bb3549898421b98d13a9e2
Mariusz Ilijew <mar_ilijew@hotmail.com>
2014-06-08 20:33:58 +0300

    added file

---
+++ FloatOrDouble.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+    class FloatOrDouble
+    {
+        static void Main()
+        {
+            double dbl_1 = 34.567839023d;
+            float fl_1 = 12.345f;
+            double dbl_2 = 8923.1234857d;
+            float fl_2 = 3456.091f;
+            Console.WriteLine("{0};  {1};  {2};  {3} ",dbl_1, fl_1, dbl_2, fl_2);
+        }
+    }
